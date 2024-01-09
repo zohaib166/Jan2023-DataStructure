03fc6e6ce328ccc79e9ad79bcf6f2c3726365b10
zohaib166 <zohaib166@gmail.com>
2024-01-09 05:40:02 +0000

    set union

---
+++ SetUnionArray.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cin >> n1 >> n2;
+    int arr1[n1];
+    int arr2[n2];
+    
+    for(int i=0;i<n1;i++) {
+        cin >> arr1[i];
+    }
+    for(int i=0;i<n2;i++) {
+        cin >> arr2[i];
+    }
+
+    int arr3[n1+n2];
+
+    int i,j,k;
+    i=j=k=0;
+    while(i<n1 && j<n2) {
+        if(arr1[i] < arr2[j]) {
+            arr3[k++] = arr1[i++];
+        } else if(arr1[i] > arr2[j]) {
+            arr3[k++] = arr2[j++];
+        } else {
+            arr3[k++] = arr1[i++];
+            j++;
+        }
+    }
+
+}
